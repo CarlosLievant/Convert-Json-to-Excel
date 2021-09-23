b0e50d033955f10b6c041ee164a22ee7595ca764
JosukeH <solodcarlos@gmai.com>
2021-09-23 14:08:02 -0500

    Se crean 2 tablas

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+const { log } = require('console');
 const fs = require('fs');
 const { Parser } = require('json2csv');
 
@@ -12,18 +13,31 @@ let data = fs.readFileSync(archivo).toString();
 concurso = boletos.map(participante => {
     return participante.split(';');
 })
- 
-console.log(concurso);
+
+boletos  =  concurso.map(participante => {
+      const boleto=participante.shift();
+      const cif=participante.shift();
+      return [boleto, cif];
+}) 
+
+
+
+console.log(boletos);
+// console.log(concurso);
+
 const concursoFormateado = {...concurso}
 
 const datosToCsv = Object.values(concursoFormateado);
 
 const json2csvParser = new Parser({});
+const json2csvParser2 = new Parser({});
 
 const csv = json2csvParser.parse(datosToCsv);
+const csv2 = json2csvParser2.parse(boletos);
 // console.log(csv);
 
-fs.writeFileSync('out/out.csv', csv);
+fs.writeFileSync('out/dbBoletos.csv', csv2);
+fs.writeFileSync('out/dbCompleta.csv', csv);
 
 
 
